22e9fbae94cd91f240446911ec0d79359683d1c9
Alexis Heijmeijer <aheijmeijer@uol.com.br>
2015-06-09 12:29:40 -0700

    Updated

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,43 +1,38 @@
 (ns euler.core
-  (:gen-class))
+  (:gen-class)
+  (require [clojure.tools.cli :refer [cli]]))
 
 (defn euler-totient-reduce
   [x]
-  (if (number? x)
-    (if (= x 1)
-      1
-      (reduce #(if (= 1 ((fn gcd
-                           [a b]
-                           (if (zero? b)
-                             a
-                             (gcd b (mod a b)))) x %2)
-                      ) (inc %) % )
-              0
-              (range 1 x)
-              ))
-    nil
-    )
-  )
+  {:pre [(number? x)]}
+  (if (= x 1)
+    1
+    (reduce #(if (= 1 ((fn gcd
+                         [a b]
+                         (if (zero? b)
+                           a
+                           (gcd b (mod a b)))) x %2)
+                    ) (inc %) %)
+            0
+            (range 1 x)
+            )))
 
 (defn euler-totient-filter
   [x]
-  (if (number? x)
-    (if (= x 1)
-      1
-      (count (filter #(= 1 ((fn gcd
-                              [a b]
-                              (if (zero? b)
-                                a
-                                (gcd b (mod a b)))) x %1)
-                         )
-                     (range 1 x)
-                     )))
-    nil
-    )
+  {:pre [(number? x)]}
+  (if (= x 1)
+    1
+    (count (filter #(= 1 ((fn gcd
+                            [a b]
+                            (if (zero? b)
+                              a
+                              (gcd b (mod a b)))) x %1)
+                       )
+                   (range 1 x)
+                   )))
   )
 
 (defn -main
   "4Clojure: Euler's Totient function"
   [& args]
-  (println (euler-totient-reduce 10))
-  (println (euler-totient-filter 50)))
+  )
--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -5,23 +5,21 @@
 (deftest euler-totient-reduce-test
   (testing "euler-totient-reduce"
     (are [n answer] (= answer (euler-totient-reduce n))
-                    () nil
-                    nil nil
                     0 0
                     1 1
                     10 (count '(1 3 7 9))
                     40 16
-                    (range 1 10) nil)
-                    ))
+                    )
+    (is (thrown? AssertionError (euler-totient-reduce nil)))
+    ))
 
 (deftest euler-totient-filter-test
   (testing "euler-totient-filter"
     (are [n answer] (= answer (euler-totient-filter n))
-                    () nil
-                    nil nil
                     0 0
                     1 1
                     10 (count '(1 3 7 9))
                     40 16
-                    (range 1 10) nil
-                    )))
+                    )
+    (is (thrown? AssertionError (euler-totient-filter [])))
+    ))
