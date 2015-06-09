658e41d7d1bbb264b772e777772ccc2e918f5255
Alexis Heijmeijer <aheijmeijer@uol.com.br>
2015-06-08 21:05:58 -0700

    Tests updated

--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -5,21 +5,23 @@
 (deftest euler-totient-reduce-test
   (testing "euler-totient-reduce"
     (are [n answer] (= answer (euler-totient-reduce n))
-                    () 0
-                    nil 0
+                    () nil
+                    nil nil
                     0 0
                     1 1
                     10 (count '(1 3 7 9))
                     40 16
-                    (range 1 10) 0)))
+                    (range 1 10) nil)
+                    ))
 
 (deftest euler-totient-filter-test
   (testing "euler-totient-filter"
     (are [n answer] (= answer (euler-totient-filter n))
-                    () 0
-                    nil 0
+                    () nil
+                    nil nil
                     0 0
                     1 1
                     10 (count '(1 3 7 9))
                     40 16
-                    (range 1 10) 0)))
+                    (range 1 10) nil
+                    )))
